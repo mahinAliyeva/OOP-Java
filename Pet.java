da3627ac1305dcb785e5fc7e3e45aeeee911ea64
mahinAliyeva <eliyeva0205@gmail.com>
2025-02-15 08:16:44 +0400

    Add files via upload

---
+++ Pet.java
@@ -0,0 +1,87 @@
+package lesson5;
+
+import java.util.Arrays;
+import java.util.Objects;
+
+public class Pet {
+    private String species;
+    private String nickname;
+    private int age;
+    private int trickLevel;
+    private String[] habits;
+
+    // Static initialization block
+    static {
+        System.out.println("Loading class: Pet");
+    }
+
+    // Non-static initialization block
+    {
+        System.out.println("Creating new Pet object");
+    }
+
+    // Constructors
+    public Pet() {}
+
+    public Pet(String species, String nickname) {
+        this.species = species;
+        this.nickname = nickname;
+    }
+
+    public Pet(String species, String nickname, int age, int trickLevel, String[] habits) {
+        this.species = species;
+        this.nickname = nickname;
+        this.age = age;
+        this.trickLevel = trickLevel;
+        this.habits = habits;
+    }
+
+    // Getters & Setters
+    public String getSpecies() { return species; }
+    public void setSpecies(String species) { this.species = species; }
+
+    public String getNickname() { return nickname; }
+    public void setNickname(String nickname) { this.nickname = nickname; }
+
+    public int getAge() { return age; }
+    public void setAge(int age) { this.age = age; }
+
+    public int getTrickLevel() { return trickLevel; }
+    public void setTrickLevel(int trickLevel) { this.trickLevel = trickLevel; }
+
+    public String[] getHabits() { return habits; }
+    public void setHabits(String[] habits) { this.habits = habits; }
+
+    // Pet Actions
+    public void eat() {
+        System.out.println("I am eating");
+    }
+
+    public void respond() {
+        System.out.println("Hello, owner. I am " + nickname + ". I miss you!");
+    }
+
+    public void foul() {
+        System.out.println("I need to cover it up");
+    }
+
+    // equals & hashCode
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Pet)) return false;
+        Pet pet = (Pet) o;
+        return age == pet.age && trickLevel == pet.trickLevel && species.equals(pet.species) && nickname.equals(pet.nickname);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(species, nickname, age, trickLevel);
+    }
+
+    // toString
+    @Override
+    public String toString() {
+        return String.format("%s{nickname='%s', age=%d, trickLevel=%d, habits=%s}", species, nickname, age, trickLevel, Arrays.toString(habits));
+    }
+}
