ab758ebbfd3fc7f512263838178d91b44cc4f4da
mahinAliyeva <eliyeva0205@gmail.com>
2025-02-22 22:23:54 +0400

    Add files via upload

---
+++ Species.java
@@ -0,0 +1,28 @@
+package lesson7;
+
+public enum Species {
+    DOG(false, 4, true),
+    CAT(false, 4, true),
+    BIRD(true, 2, false),
+    FISH(false, 0, false),
+    SPIDER(false, 8, false);
+
+    private final boolean canFly;
+    private final int numberOfLegs;
+    private final boolean hasFur;
+
+    Species(boolean canFly, int numberOfLegs, boolean hasFur) {
+        this.canFly = canFly;
+        this.numberOfLegs = numberOfLegs;
+        this.hasFur = hasFur;
+    }
+
+    public boolean canFly() { return canFly; }
+    public int getNumberOfLegs() { return numberOfLegs; }
+    public boolean hasFur() { return hasFur; }
+
+    @Override
+    public String toString() {
+        return String.format("%s{canFly=%b, numberOfLegs=%d, hasFur=%b}", name(), canFly, numberOfLegs, hasFur);
+    }
+}
