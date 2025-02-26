8e001024fdd0505e654d5288a788d001dc8a8dea
mahinAliyeva <eliyeva0205@gmail.com>
2025-02-26 21:19:13 +0400

    Update Main.java

--- lesson10/Main.java
+++ lesson10/Main.java
@@ -6,27 +6,41 @@ public class Main {
         Manager manager2 = new Manager("Bob", 102, 8);
         Developer developer1 = new Developer("Charlie", 201, "Java");
         Developer developer2 = new Developer("David", 202, "Python");
+        Designer designer1 = new Designer("Emma", 301, "Figma");
+        Designer designer2 = new Designer("Sophia", 302, "Adobe XD");
 
         manager1.displayInfo();
         manager1.work();
         manager1.conductMeeting();
-
+        
         System.out.println();
-
+        
         manager2.displayInfo();
         manager2.work();
         manager2.conductMeeting();
-
+        
         System.out.println();
-
+        
         developer1.displayInfo();
         developer1.work();
         developer1.debugCode();
-
+        
         System.out.println();
-
+        
         developer2.displayInfo();
         developer2.work();
         developer2.debugCode();
+        
+        System.out.println();
+        
+        designer1.displayInfo();
+        designer1.work();
+        designer1.createMockups();
+        
+        System.out.println();
+        
+        designer2.displayInfo();
+        designer2.work();
+        designer2.createMockups();
     }
 }
